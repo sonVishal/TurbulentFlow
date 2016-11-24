ec394a3b3860e9a569e110bbb07881d4405452b8
Michael Zellner <zellnerm@in.tum.de>
2016-11-24 13:20:00 +0100

    Added VTKMPIStencil for debug mpi with paraview. Use Iterator with low_offset=-1 and high_offset=1

---
+++ stencils/VTKMPIStencil.cpp
@@ -0,0 +1,144 @@
+#include "VTKMPIStencil.h"
+
+/** Constructor
+ *
+ * @param prefix String with the prefix of the name of the VTK files
+ */
+VTKMPIStencil::VTKMPIStencil ( const Parameters & parameters ) : FieldStencil<FlowField> ( parameters ) {
+    // Get the local size and first corner of this subdomain
+    _localSize   = parameters.parallel.localSize;
+
+
+
+    _pressureStream << std::setprecision(6);
+    _velocityStream << std::setprecision(6);
+}
+
+/** 2D operation for one position
+ *
+ * @param flowField State of the flow field
+ * @param i Position in the x direction
+ * @param j Position in the y direction
+ */
+void VTKMPIStencil::apply ( FlowField & flowField, int i, int j ) {
+    // Since the field iterator includes the left and bottom ghost layers we skip them
+
+    // Check if fluid cell and then print out to the stringstreams
+    if ((flowField.getFlags().getValue(i,j) & OBSTACLE_SELF) == 0) {
+        FLOAT pressure;
+        FLOAT velocity[2];
+        flowField.getPressureAndVelocity(pressure, velocity, i, j);
+        _pressureStream << pressure << std::endl;
+        _velocityStream << velocity[0] << ' ' << velocity[1] << ' ' << 0 << std::endl;
+    } else {
+    // Otherwise output 0 values
+        _pressureStream << 0 << std::endl;
+        _velocityStream << 0 << ' ' << 0 << ' ' << 0 << std::endl;
+    }
+}
+
+/** 3D operation for one position
+ *
+ * @param flowField State of the flow field
+ * @param i Position in the x direction
+ * @param j Position in the y direction
+ * @param k Position in the z direction
+ */
+void VTKMPIStencil::apply ( FlowField & flowField, int i, int j, int k ) {
+    // Since the field iterator include the left, bottom and front ghost layers we skip them
+
+    // Check if fluid cell and then print out to the stringstreams
+    if ((flowField.getFlags().getValue(i,j,k) & OBSTACLE_SELF) == 0) {
+        FLOAT pressure;
+        FLOAT velocity[3];
+        flowField.getPressureAndVelocity(pressure, velocity, i, j, k);
+        _pressureStream << pressure << std::endl;
+        _velocityStream << velocity[0] << ' ' << velocity[1] << ' ' << velocity[2] << std::endl;
+    } else {
+    // Otherwise output 0 values
+        _pressureStream << 0 << std::endl;
+        _velocityStream << 0 << ' ' << 0 << ' ' << 0 << std::endl;
+    }
+}
+
+/** Writes the header for the VTK file as well as the coordinates
+ */
+void VTKMPIStencil::writeHeaderAndCoords() {
+    // This is the header
+    _outputFile << "# vtk DataFile Version 2.0\n" << "I need something to put here\n";
+    _outputFile << "ASCII\n" << std::endl;
+
+    // Write the header for the coordinates
+    // Number of coordinates is 1 more than the number of cells in that direction
+    _outputFile << "DATASET STRUCTURED_GRID\n" << "DIMENSIONS ";
+    _outputFile << _localSize[0]+4 << ' ' << _localSize[1]+4 << ' ' << _localSize[2]+1 << std::endl;
+    _outputFile << "POINTS " << (_localSize[0]+4)*(_localSize[1]+4)*(_localSize[2]+1) << " float" << std::endl;
+
+    // Temporary variables to avoid calling function many times
+    FLOAT y = 0.0, z = 0.0;
+
+    // Call for loop depending on dimension
+    // Start from 2 as we skip the ghost cells (both lower and upper)
+    if (_parameters.geometry.dim == 2) {
+        for (int j = 0; j < _localSize[1]+4; j++) {
+            for (int i = 0; i < _localSize[0]+4; i++) {
+                _outputFile << _parameters.meshsize->getPosX(i,j);
+                _outputFile << ' ' << _parameters.meshsize->getPosY(i,j) << ' ' << z << std::endl;
+            }
+        }
+    } else {
+        for (int k = 2; k < _localSize[2]+3; k++) {
+            z = _parameters.meshsize->getPosZ(2,2,k);
+            for (int j = 2; j < _localSize[1]+3; j++) {
+                y = _parameters.meshsize->getPosY(2,j,k);
+                for (int i = 2; i < _localSize[0]+3; i++) {
+                    _outputFile << _parameters.meshsize->getPosX(i,j,k);
+                    _outputFile << ' ' << y << ' ' << z << std::endl;
+                }
+            }
+        }
+    }
+}
+
+/** Opens the file for writing and returns true if it was opened otherwise false
+ * returns false if file was not opened or memory was not allocated for
+ * pressure and velocity streams.
+ * @param timeStep used to create the file name for current time step
+ */
+bool VTKMPIStencil::openFile( int timeStep ) {
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    // fileName stores the file name as per XML file
+    // it adds current timestep + .vtk extension to it
+    std::stringstream fileName;
+    fileName << _parameters.vtk.prefix << "_All"<<'_'<<"rank_"<<rank<<"_" << timeStep << ".vtk";
+
+    // Open the file for writing out
+    _outputFile.open(fileName.str().c_str(),std::ios::out);
+
+    return _outputFile.is_open();
+}
+
+/** Writes the information to the file
+ * @param flowField Flow field to be written
+ */
+void VTKMPIStencil::write ( FlowField & flowField, int timeStep ) {
+    // Write the header and coordinates
+    writeHeaderAndCoords();
+    // Write the header for pressure field
+    _outputFile << "\nCELL_DATA " << (_localSize[0]+3)*(_localSize[1]+3)*(_localSize[2]==0?1:_localSize[2]) << std::endl;
+    _outputFile << "SCALARS pressure float 1\n";
+    _outputFile << "LOOKUP_TABLE default\n";
+    // Write the pressure
+    _outputFile << _pressureStream.str();
+    // Write the header for velocity field
+    _outputFile << "\nVECTORS velocity float\n";
+    // Write the velocity
+    _outputFile << _velocityStream.str();
+    // Close the file
+    _outputFile.close();
+
+    // Clear the stringstream buffers
+    _pressureStream.str(std::string());
+    _velocityStream.str(std::string());
+}
---
+++ stencils/VTKMPIStencil.h
@@ -0,0 +1,79 @@
+#ifndef _VTK_ALL_STENCIL_H_
+#define _VTK_ALL_STENCIL_H_
+
+#include "../Definitions.h"
+#include "../Parameters.h"
+#include "../Stencil.h"
+#include "../FlowField.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+
+/** Stencil for writting VTK files
+ *
+ * When iterated with, creates a VTK file.
+ */
+class VTKMPIStencil : public FieldStencil<FlowField> {
+    private:
+
+        /** File stream variable used for writing the VTK file
+         */
+        std::ofstream _outputFile;
+
+        /** String stream variable used for storing pressure values at current time
+         * As per my knowledge the stream buffers are internally allocated on the heap
+         */
+        std::ostringstream _pressureStream;
+
+        /** String stream variable used for storing velocity values at current time
+         * As per my knowledge the stream buffers are internally allocated on the heap
+         */
+        std::ostringstream _velocityStream;
+
+        /** Local size of the mesh in X,Y, and Z direction
+         */
+        const int* _localSize;
+
+        /** Writes the header for the VTK file as well as the coordinates
+         */
+        void writeHeaderAndCoords();
+
+    public:
+
+        /** Constructor
+         *
+         * @param prefix String with the prefix of the name of the VTK files
+         */
+        VTKMPIStencil ( const Parameters& parameters );
+
+        /** 2D operation for one position
+         *
+         * @param flowField State of the flow field
+         * @param i Position in the x direction
+         * @param j Position in the y direction
+         */
+        void apply ( FlowField & flowField, int i, int j );
+
+        /** 3D operation for one position
+         *
+         * @param flowField State of the flow field
+         * @param i Position in the x direction
+         * @param j Position in the y direction
+         * @param k Position in the z direction
+         */
+        void apply ( FlowField & flowField, int i, int j, int k );
+
+        /** Writes the information to the file
+         * @param flowField Flow field to be written
+         */
+        void write ( FlowField & flowField, int timeStep );
+
+        /** Opens the file for writing and returns true if it was opened otherwise false
+         * @param timeStep used to create the file name for current time step
+         */
+        bool openFile ( int timeStep );
+
+};
+
+#endif

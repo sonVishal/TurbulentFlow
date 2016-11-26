1be210ad920be7861d9638dbc4e3ba2f8064d6a2
Vishal Sontakke <vishal.sontakke@tum.de>
2016-11-26 19:09:17 +0100

    Fix channel with step

--- TurbulentSimulation.h
+++ TurbulentSimulation.h
@@ -1,7 +1,7 @@
 #ifndef _TURBULENT_SIMULATION_H_
 #define _TURBULENT_SIMULATION_H_
 #include "Simulation.h"
-
+#include "TurbFlowField.h"
 class TurbulentSimulation : public Simulation {
 private:
     /* TODO */
